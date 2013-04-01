7b2962d606a0a272be85ab18f59cca6a6d2c9ac6
lusaisai <lusaisai@163.com>
2013-04-01 19:16:27 +0800

    backup

---
+++ genTool/latest_html/js/topic_92.js
@@ -0,0 +1,3 @@
+$( function() {
+$('#list433').click(function() {$('#topic92 span.listtxt').html('春晚我就记得这一首歌。李健可以唱得更好，估计在孙俪旁边紧张了～～');$('#topic92 embed').wrap('<div class="videoWrapper" />');$('#topic92 embed').replaceWith('<embed src="http://player.yinyuetai.com/video/player/604526/v_0.swf" quality="high" width="680" height="480" align="middle"  allowScriptAccess="sameDomain" allowfullscreen="true" type="application/x-shockwave-flash"></embed>');$('#topic92 li').css('color', '#000000');$('#list433').css('color', '#CC0052');});
+});
---
+++ genTool/latest_html/js/topic_93.js
@@ -0,0 +1,3 @@
+$( function() {
+$('#list438').click(function() {$('#topic93 span.listtxt').html('我觉得挺好听的，主要是有几个女生太漂亮了～～');$('#topic93 embed').wrap('<div class="videoWrapper" />');$('#topic93 embed').replaceWith('<embed src="http://player.yinyuetai.com/video/player/599055/v_0.swf" quality="high" width="680" height="480" align="middle"  allowScriptAccess="sameDomain" allowfullscreen="true" type="application/x-shockwave-flash"></embed>');$('#topic93 li').css('color', '#000000');$('#list438').css('color', '#CC0052');});
+});
